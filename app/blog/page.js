eb168e91dcc3aed7289d882b05cc20e4efa83d04
ifotn <Rich.Freeman@GeorgianCollege.ca>
2024-10-29 13:19:13 -0400

    l8/2 - split blog into client & server

--- app/blog/page.js
+++ app/blog/page.js
@@ -1,27 +1,7 @@
-import Link from "next/link";
+import BlogServer from "../components/blogServer";
 
 export default async function Blog() {
-    // make API call to the vercel blog to retrieve its contents
-    let data = await fetch('https://api.vercel.app/blog');
-    let posts = await data.json();
-    //console.log(posts);
-
     return (
-        <main>
-            <h1>Blog</h1>
-            <p>This is a demo blog using the Fetch API and data from https://api.vercel.app/blog</p>
-            <ul className="list-group">
-                {posts.map((post) => (
-                    <li key={post.id} className="list-group-item">
-                        <h4>
-                            <Link href={`/blog/${post.id}`}>
-                                {post.title}
-                            </Link>                                
-                        </h4>
-                        {post.author} - {post.date}
-                    </li>
-                ))}
-            </ul>
-        </main>
+        <BlogServer />
     )
 }
---
+++ app/components/blogServer.js
@@ -0,0 +1,14 @@
+import BlogClient from "./blogClient";
+
+// we can use async to call the api as there's no use client here.
+// this is a next server component instead 
+export default async function BlogServer() {
+    // make API call to the vercel blog to retrieve its contents
+    let data = await fetch('https://api.vercel.app/blog');
+    let posts = await data.json();
+
+    // render blog client and pass all blog json as a prop
+    return (
+        <BlogClient posts={posts} />
+    );
+}
